fb1cf81de02924ddc94a96037802e06f29119f84
Caique S. Soares <caiqueoucaique@hotmail.com>
2025-10-22 14:42:08 -0300

    Fix migration metadata for Ambos species option

--- backend/src/PremieRpet.Shop.Infrastructure/Migrations/20251025070000_AddProdutoEspecieAmbos.cs
+++ backend/src/PremieRpet.Shop.Infrastructure/Migrations/20251025070000_AddProdutoEspecieAmbos.cs
@@ -1,11 +1,16 @@
 using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
+using PremieRpet.Shop.Infrastructure;
 
 #nullable disable
 
 namespace PremieRpet.Shop.Infrastructure.Migrations
 {
     /// <inheritdoc />
+    [DbContext(typeof(ShopDbContext))]
+    [Migration("20251025070000_AddProdutoEspecieAmbos")]
     public partial class AddProdutoEspecieAmbos : Migration
     {
         /// <inheritdoc />
@@ -24,6 +29,17 @@ namespace PremieRpet.Shop.Infrastructure.Migrations
                 table: "ProdutoEspecieOpcoes",
                 keyColumn: "Id",
                 keyValue: new Guid("4e45286c-5cf4-4b86-9a3e-6a620fc7b3f9"));
+        }
+
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            new ShopDbContextModelSnapshotProxy().BuildModel(modelBuilder);
+        }
+
+        private sealed class ShopDbContextModelSnapshotProxy : ShopDbContextModelSnapshot
+        {
+            public void BuildModel(ModelBuilder modelBuilder) => base.BuildModel(modelBuilder);
         }
     }
 }
